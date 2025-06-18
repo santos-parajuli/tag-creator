43615af3af15c78f56d3609d7c8ac2acf681a660
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2025-06-18 09:33:49 -0400

    Update Tag.jsx

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -3,47 +3,47 @@ import React from 'react';
 
 const Tag = ({ fabric, labelType }) => {
 	return (
-		<div className='w-[15.5cm] h-[8.5cm] border border-gray-300 font-sans flex m-auto'>
+		<div className='w-[15.5cm] h-[8.5cm] border border-gray-300 font-sans flex m-auto text-gray-900'>
 			{/* Vertical Fabric Type label */}
 			<div className='bg-zinc-300 p-4 text-zinc-400 flex items-center justify-center w-[1.4cm]'>
-				<h2 className='uppercase text-[1.7rem] whitespace-nowrap transform -rotate-90 origin-center'>{labelType}</h2>
+				<h2 className='uppercase text-[16.3pt] whitespace-nowrap transform -rotate-90 origin-center'>{labelType}</h2>
 			</div>
 
 			{/* Main content */}
 			<div className=' pl-[0.5cm] w-[9cm] flex flex-col justify-between tracking-wide'>
 				<div>
 					{/* Name and Color */}
-					<div className='text-xl my-3 tracking-wider'>
+					<div className='text-[12.75pt] my-5 tracking-wider'>
 						<h1 className='font-medium uppercase'>
 							{fabric.fabric},<span className='font-normal capitalize'> {fabric.color}</span>
 						</h1>
 					</div>
 
 					{/* Content rows with right-aligned labels */}
-					<div className='flex text-xs my-[0.1cm]'>
+					<div className='flex text-[7.5pt] my-[0.1cm]'>
 						<span className='w-22 text-right mr-2'>CONTENT</span>
 						<span className='font-medium'>{fabric.content}</span>
 					</div>
-					<div className='flex text-xs my-[0.1cm]'>
+					<div className='flex text-[7.5pt] my-[0.1cm]'>
 						<span className='w-22 text-right mr-2'>DOUBLE RUBS</span>
 						<span className='font-medium'>{fabric.rubs}</span>
 					</div>
-					<div className='flex text-xs my-[0.1cm]'>
+					<div className='flex text-[7.5pt] my-[0.1cm]'>
 						<span className='w-22 text-right mr-2'>WIDTH</span>
 						<span className='font-medium'>{fabric.width}</span>
 					</div>
 
 					{/* Usage */}
-					<div className='flex text-xs mt-3'>
+					<div className='flex text-[7.5pt] mt-3'>
 						<div className='flex flex-col items-end mr-2'>
 							<span className='w-22 text-right'>USAGE</span>
 							<div className='h-[2cm] w-[2cm]  mt-1'>
 								<QRCode fgColor='#000000' value={'https://www.tonicliving.ca/'} size={75} />
 							</div>
 						</div>
-						<div className=' font-medium flex flex-wrap flex-col text-xs gap-1 mb-[0.3cm]'>
+						<div className=' font-medium flex flex-wrap flex-col text-[7.5pt] gap-1 mb-[0.3cm]'>
 							{['Drapery', 'Upholstery', 'Romans', 'High-Performance', 'Pillows'].map((item) => (
-								<div key={item} className='flex flex-row items-center'>
+								<div key={item} className='flex flex-row items-center '>
 									<div className='w-2 h-2 mr-1'>
 										<img src='/Cross.svg' className='w-2 h-2 mr-1' alt='checkbox' />
 									</div>
